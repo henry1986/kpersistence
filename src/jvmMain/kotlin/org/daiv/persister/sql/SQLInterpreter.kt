f122bcb0cea4e04fe4d62e9346de0dcdc6945fca
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2022-01-09 14:21:01 +0100

    ...

--- src/jvmMain/kotlin/org/daiv/persister/sql/SQLInterpreter.kt
+++ src/jvmMain/kotlin/org/daiv/persister/sql/SQLInterpreter.kt
@@ -1,9 +1,15 @@
 package org.daiv.persister.sql
 
 import org.daiv.persister.objectrelational.CormMap
+import org.daiv.persister.objectrelational.HashCodeCounterGetter
 import org.daiv.persister.objectrelational.ObjectRelationalMapper
 
-class SQLInterpreter<T : Any>(val objectRelationalMapper: ObjectRelationalMapper<T>, val cormMap: CormMap) {
+
+class SQLInterpreter<T : Any>(
+    val objectRelationalMapper: ObjectRelationalMapper<T>,
+    val cormMap: CormMap,
+    val hashCodeCounterGetter: HashCodeCounterGetter = HashCodeCounterGetter.nullGetter
+) {
 
     fun select(tableName: String, propertyNames: List<String>): String {
         val ret = objectRelationalMapper.objectRelationalHeader.allHeads(null, null)
@@ -11,5 +17,17 @@ class SQLInterpreter<T : Any>(val objectRelationalMapper: ObjectRelationalMapper
             .joinToString(" AND ") { "${it.name} = ?" }
         return "SELECT * FROM $tableName WHERE $ret;"
     }
+
+    fun insert(ts: List<T>): Pair<String, List<String>> {
+        if (ts.isEmpty()) {
+            return "" to emptyList()
+        }
+        val writer = objectRelationalMapper.objectRelationalWriter
+        val rows = ts.map { writer.writeRow(null, it, hashCodeCounterGetter) }
+        val names = rows.first().joinToString(", ") { "${it.name}" }
+        val values = rows.map { row -> row.joinToString(", ") { "${it.value}" } }
+        return names to values
+    }
+
 
 }
--- src/jvmTest/kotlin/org/daiv/persister/sql/SQIInterpreterTester.kt
+++ src/jvmTest/kotlin/org/daiv/persister/sql/SQIInterpreterTester.kt
@@ -1,10 +1,11 @@
 package org.daiv.persister.sql
 
-import org.daiv.persister.objectrelational.CormMap
-import org.daiv.persister.objectrelational.HashCodeCounterGetter
+import org.daiv.persister.MoreKeys
+import org.daiv.persister.objectrelational.*
 import org.daiv.persister.runTest
 import org.junit.Test
 import java.sql.DriverManager
+import kotlin.reflect.KClass
 import kotlin.test.assertEquals
 
 class SQIInterpreterTester {
@@ -35,6 +36,9 @@ class SQIInterpreterTester {
     }
 
     private data class XP(val x1: Int, val x2: String)
+    @MoreKeys(2)
+    private data class XP2(val x1: Int, val x2: String, val x3:Int)
+    private data class XPHandler(val x1: Int, val x2: XP, val xp2: XP2)
 
     @Test
     fun test() = runTest {
@@ -42,14 +46,29 @@ class SQIInterpreterTester {
         val driver = DriverManager.getConnection("jdbc:sqlite:$path")
         val create = "CREATE TABLE IF NOT EXISTS XP (x1 INT NOT NULL, x2 String NOT NULL, PRIMARY KEY(x1));"
 
-        val x = cormMap.getValue(XP::class)
+        val x = cormMap.getValue(XPHandler::class)
+        val sqlInterpreter = SQLInterpreter(x, cormMap)
+        val xpHandler1 = XPHandler(5, XP(9, "Blub"), XP2(1, "h1", 19))
+        val xpHandler2 = XPHandler(3, XP(36, "Blub36"), XP2(2, "h2", 29))
+        val insert = sqlInterpreter.insert(listOf(xpHandler1, xpHandler2))
         val h = HashCodeCounterGetter.nullGetter
-        val d =  x.objectRelationalWriter.writeRow(null, XP(5, "Blub"), h)
-        val join = d.joinToString(", ") { "${it.name}" }
-        val values = d.joinToString(", ") { "${it.value}" }
-        println("join: $join")
-        println("values: $values")
-        println("writeKey: $d")
+        x.objectRelationalWriter.subs(xpHandler1, object : TaskReceiver{
+            override suspend fun <R> task(r: R, higherKeys: List<WriteEntry>, mapper: ObjectRelationalWriter<R>) {
+                r?.let {
+                    val c = it::class as KClass<Any>
+                    val sqlInterpreter = SQLInterpreter(cormMap.getValue(c), cormMap)
+                    val ret = sqlInterpreter.insert(listOf(r))
+                    println("writeKeySub: ${ret.first}")
+                    ret.second.forEach {
+                        println("itSub: $it")
+                    }
+                }
+            }
+        }, h)
+        println("writeKey: ${insert.first}")
+        insert.second.forEach {
+            println("it: $it")
+        }
 //        val insert = "insert into XP (x1, x2) values (?, ?), (?, ?);"
 //        val st = driver.prepareStatement(insert)
 //        st.setInt(1, 5)
@@ -58,5 +77,35 @@ class SQIInterpreterTester {
 //        st.setString(4, "Wow")
 //        st.execute()
 //        st.close()
+    }
+
+    private data class ListHolder(val x1: Int, val l:List<String>)
+
+    @Test
+    fun testSQLList() = runTest{
+        val x = cormMap.getValue(ListHolder::class)
+        val sqlInterpreter = SQLInterpreter(x, cormMap)
+        val holder = ListHolder(5, listOf("Hello", "World"))
+        val insert = sqlInterpreter.insert(listOf(holder))
+
+        println("insert: ${insert.first}")
+        insert.second.forEach {
+            println("it: $it")
+        }
+
+        x.objectRelationalWriter.subs(holder, object : TaskReceiver{
+            override suspend fun <R> task(r: R, higherKeys: List<WriteEntry>, mapper: ObjectRelationalWriter<R>) {
+                r?.let {
+                    val c = it::class as KClass<Any>
+                    val sqlInterpreter = SQLInterpreter(cormMap.getValue(c), cormMap)
+                    val ret = sqlInterpreter.insert(listOf(r))
+                    println("writeKeySub: ${ret.first}")
+                    ret.second.forEach {
+                        println("itSub: $it")
+                    }
+                }
+            }
+        }, HashCodeCounterGetter.nullGetter)
+
     }
 }
