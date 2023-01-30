0fbaf795829c02a08ec3de773d52baf51a683c1f
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2023-01-30 12:41:19 +0100

    ..

--- src/commonTest/kotlin/org/daiv/persister/sql/command/PropertySelectKeyTest.kt
+++ src/commonTest/kotlin/org/daiv/persister/sql/command/PropertySelectKeyTest.kt
@@ -1,7 +1,10 @@
 package org.daiv.persister.sql.command
 
 import org.daiv.persister.ObjectTypeHandlerTest
+import org.daiv.persister.ObjectTypeHandlerTest.MyObject
 import org.daiv.persister.TestComplexObjectType
+import org.daiv.persister.TestComplexObjectType.ComplexObject
+import org.daiv.persister.complexObjectTypeHandler
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -9,10 +12,27 @@ class PropertySelectKeyTest {
 
     @Test
     fun testForKey(){
-        val l = 5.forKey(TestComplexObjectType.ComplexObject::m, ObjectTypeHandlerTest.MyObject::i)
-        val l2 = ObjectTypeHandlerTest.MyObject(5, "Hello", 9L).forKey(TestComplexObjectType.ComplexObject::m)
+        val l = 5.forKey(ComplexObject::m, MyObject::i)
+        val l2 = MyObject(5, "Hello", 9L).forKey(ComplexObject::m)
 
         assertEquals(SelectKey(listOf("m", "i"), 5),l.toSelectKey())
-        assertEquals(SelectKey(listOf("m"), ObjectTypeHandlerTest.MyObject(5, "Hello", 9L)),l2.toSelectKey())
+        assertEquals(SelectKey(listOf("m"), MyObject(5, "Hello", 9L)),l2.toSelectKey())
+
+    }
+
+    @Test
+    fun testWithoutConjunction(){
+        val c = ComplexObject(MyObject(5, "Hello", 9L), 15, "World")
+        val typeHandler = complexObjectTypeHandler
+        val l: SelectKey = 5.forKey(ComplexObject::m, MyObject::i).toSelectKey()
+        val expect = "select * from ComplexObject where m_i = 5;"
+    }
+    @Test
+    fun testWithConjunction(){
+        val c = ComplexObject(MyObject(5, "Hello", 9L), 15, "World")
+        val typeHandler = complexObjectTypeHandler
+        val l2: SelectKey = MyObject(5, "Hello", 9L).forKey(ComplexObject::m).toSelectKey()
+        val expect = "select * from ComplexObject where m_i = 5 AND m_s=\"Hello\";"
+        
     }
 }
